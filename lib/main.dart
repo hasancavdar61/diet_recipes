fa0e90cc2871e25a256bc0f234ce00dab17e28ab
hasancavdar61 <hasancan_cavdar@hotmail.com>
2022-06-23 08:57:54 +0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:diet_recipes/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const DietApp());
+}
+
+class DietApp extends StatelessWidget {
+  const DietApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
